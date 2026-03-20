6168e8c49520483969c1185e4c68dddcada696f7
Yuri Santos <yuridssantos92@gmail.com>
2026-03-20 18:53:43 -0300

    chore: Adicionar configurações iniciais do banco de dados fornecido para o desafio

---
+++ src/BibliotecaApi/Infrastructure/Data/DbSession.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace BibliotecaApi.Infrastructure.Data
+{
+    public class DbSession : IDisposable
+    {
+        public IDbConnection Connection { get; }
+        public IDbTransaction? Transaction { get; set; }
+
+        public DbSession(IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("Database");
+
+            bool connectionStringInvalida = string.IsNullOrWhiteSpace(connectionString);
+            if (connectionStringInvalida)
+            {
+                throw new InvalidOperationException("Connection string 'Database' não encontrada.");
+            }
+
+            Connection = new SqliteConnection(connectionString);
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            Transaction?.Dispose();
+            Connection.Dispose();
+        }
+    }
+}
