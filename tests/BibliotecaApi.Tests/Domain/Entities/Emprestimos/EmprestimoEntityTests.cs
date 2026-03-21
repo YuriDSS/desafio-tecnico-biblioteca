68bc6e0504daae5336a3999f5388a5ac06477a92
Yuri Santos <yuridssantos92@gmail.com>
2026-03-21 09:18:42 -0300

    refactor: Reorganização da arquitetura de testes

---
+++ tests/BibliotecaApi.Tests/Domain/Entities/Emprestimos/EmprestimoEntityTests.cs
@@ -0,0 +1,58 @@
+﻿using BibliotecaApi.Domain.Entities;
+
+namespace BibliotecaApi.Tests.Domain.Entities.Emprestimos
+{
+    public class EmprestimoEntityTests
+    {
+        [Fact]
+        public void RegistrarDevolucao_QuandoDevolvidoNoPrazo_DeveGerarMultaZero()
+        {
+            EmprestimoEntity emprestimo = new();
+            DateTime dataPrevista = DateTime.Now.AddDays(1);
+
+            emprestimo.Cadastrar(idUsuario: 1, idLivro: 1, dataPrevista: dataPrevista);
+
+            DateTime dataDevolucao = dataPrevista;
+
+            emprestimo.RegistrarDevolucao(dataDevolucao);
+
+            Assert.Equal(0m, emprestimo.Multa);
+            Assert.Equal(5.00m, emprestimo.Total);
+            Assert.Equal(dataDevolucao, emprestimo.DataDevolucao);
+        }
+
+        [Fact]
+        public void RegistrarDevolucao_QuandoDevolvidoAntesDoPrazo_DeveGerarMultaZero()
+        {
+            EmprestimoEntity emprestimo = new();
+            DateTime dataPrevista = DateTime.Now.AddDays(3);
+
+            emprestimo.Cadastrar(idUsuario: 1, idLivro: 1, dataPrevista: dataPrevista);
+
+            DateTime dataDevolucao = dataPrevista.AddDays(-1);
+
+            emprestimo.RegistrarDevolucao(dataDevolucao);
+
+            Assert.Equal(0m, emprestimo.Multa);
+            Assert.Equal(5.00m, emprestimo.Total);
+            Assert.Equal(dataDevolucao, emprestimo.DataDevolucao);
+        }
+
+        [Fact]
+        public void RegistrarDevolucao_QuandoDevolvidoComAtraso_DeveGerarMultaCorreta()
+        {
+            EmprestimoEntity emprestimo = new();
+            DateTime dataPrevista = DateTime.Now.AddDays(3);
+
+            emprestimo.Cadastrar(idUsuario: 1, idLivro: 1, dataPrevista: dataPrevista);
+
+            DateTime dataDevolucao = dataPrevista.AddDays(2);
+
+            emprestimo.RegistrarDevolucao(dataDevolucao);
+
+            Assert.Equal(4.00m, emprestimo.Multa);
+            Assert.Equal(9.00m, emprestimo.Total);
+            Assert.Equal(dataDevolucao, emprestimo.DataDevolucao);
+        }
+    }
+}
